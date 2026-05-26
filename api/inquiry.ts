54aa71f2cbc2b7d062cbf8e57ca7ac98c9b65f90
Pratyush Mishra <commercialop987@gmail.com>
2026-05-26 15:53:27 +0530

    Add CORS headers to API

--- api/inquiry.ts
+++ api/inquiry.ts
@@ -2,6 +2,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
